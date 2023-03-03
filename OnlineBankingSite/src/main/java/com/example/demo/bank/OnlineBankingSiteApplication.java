598382119a000beaa37b0aac797809a15f2ad5c1
dell <dell@DESKTOP-GSCM8DR>
2023-03-03 14:46:57 +0530

    Initial commit

---
+++ OnlineBankingSite/src/main/java/com/example/demo/bank/OnlineBankingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingSiteApplication.class, args);
+	}
+
+}
